cb552ac96c09c4f524999ea4525f2f3be53578e6
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-16 11:19:49 -0600

    commit tree 48fb8f944b0d21ad9094e2c3fcab11fc473aa4e4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'Lucas Sweeney'
+const upperCaseCurrentUser = currentUser.toUpperCase()
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!'
+
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!'
+
